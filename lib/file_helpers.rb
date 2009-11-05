609241e20eb8cc3bc3e62f760ff0a6bfe423f0bc
Scott Taylor <scott@railsnewbie.com>
2009-11-05 17:24:13 -0500

    Add trailing whitesapce cleaner

--- lib/file_helpers.rb
+++ lib/file_helpers.rb
@@ -10,6 +10,10 @@ class FileHelpers
     def remove_hard_tabs(start_dir = Dir.getwd)
       new(start_dir).remove_hard_tabs
     end
+    
+    def clean_whitespace(start_dir = Dir.getwd)
+      new(start_dir).clean_whitespace
+    end
   end
 
   def initialize(start_dir = Dir.getwd)
@@ -25,6 +29,27 @@ class FileHelpers
   def remove_hard_tabs
     remove_all(/\t/, "  ") do |file|
       puts "Removing hard tabs from #{file}"
+    end
+  end
+  
+  def clean_whitespace
+    files = Dir.glob("#{@start_dir}/**/*")
+
+    files.each do |file|
+      if File.file?(file)
+        
+        contents = File.read(file)
+        
+        File.open(file, "w") do |f|
+          new_str = ""
+          
+          StringIO.new(contents).readlines.each do |line|
+            new_str << line.rstrip + "\n"
+          end
+          
+          f << new_str
+        end
+      end
     end
   end
   
