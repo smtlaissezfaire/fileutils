fcfef3a6061d13529cfb45fd3f9748ef8f965e94
Scott Taylor <scott@railsnewbie.com>
2010-09-02 16:54:05 -0400

    - require stringio for ruby 1.8.7 - remove require 'rubygems'

--- lib/file_helpers.rb
+++ lib/file_helpers.rb
@@ -1,16 +1,16 @@
-require "rubygems"
 require "facets/file/rewrite"
+require "stringio"
 
 class FileHelpers
   class << self
     def remove_carriage_returns(start_dir = Dir.getwd)
       new(start_dir).remove_carriage_returns
     end
-    
+
     def remove_hard_tabs(start_dir = Dir.getwd)
       new(start_dir).remove_hard_tabs
     end
-    
+
     def clean_whitespace(start_dir = Dir.getwd)
       new(start_dir).clean_whitespace
     end
@@ -31,38 +31,38 @@ class FileHelpers
       puts "Removing hard tabs from #{file}"
     end
   end
-  
+
   def clean_whitespace
     files = Dir.glob("#{@start_dir}/**/*")
 
     files.each do |file|
       if File.file?(file)
-        
+
         contents = File.read(file)
-        
+
         File.open(file, "w") do |f|
           new_str = ""
-          
+
           StringIO.new(contents).readlines.each do |line|
             new_str << line.rstrip + "\n"
           end
-          
+
           f << new_str
         end
       end
     end
   end
-  
+
 private
-  
+
   def remove_all(search, replace)
     files = Dir.glob("#{@start_dir}/**/*")
 
     files.each do |file|
       if File.file?(file)
-        
+
         yield(file)
-        
+
         File.rewrite(file) do |contents|
           str = contents.dup
           str.gsub!(search, replace)
